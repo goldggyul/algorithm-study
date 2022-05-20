ced0be32621b932c458864e9cacaf302f7b932ee
sohyun <kshyun0215@gmail.com>
2022-05-21 01:02:38 +0900

    solve: programmers lv.1 - 없는 숫자 더하기 (#4)

---
+++ programmers/sohyun/level1/없는_숫자_더하기/Solution.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = -1;
+    int sum = 0;
+    for(int n : numbers)
+        sum += n;
+    answer =  45 - sum;
+    return answer;
+}
