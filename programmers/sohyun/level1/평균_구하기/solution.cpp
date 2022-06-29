d5365d735a9b0b01448ba51309298ac10db11a88
sohyun <kshyun0215@gmail.com>
2022-06-29 18:38:17 +0900

    solve: programmers lv.1 - 평균 구하기 (#4)

---
+++ programmers/sohyun/level1/평균_구하기/solution.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    double sum = 0;
+    for(int a: arr) sum += a;
+    answer = sum / arr.size();
+    return answer;
+}
