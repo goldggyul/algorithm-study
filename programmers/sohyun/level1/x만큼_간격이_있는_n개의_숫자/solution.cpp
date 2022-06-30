cf1edf11f99764d872c444953b0c9c1d3a47344a
sohyun <kshyun0215@gmail.com>
2022-07-01 01:11:06 +0900

    solve: programmers lv.1 - x만큼 간격이 있는 n개의 숫자 (#4)

---
+++ programmers/sohyun/level1/x만큼_간격이_있는_n개의_숫자/solution.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int num = x;
+    while(n--){
+        answer.push_back(x);
+        x += num;
+    }
+   
+    return answer;
+}
