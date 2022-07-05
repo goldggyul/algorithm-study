5bff595dfa4f10c9dda5ea2273ff701cb686793e
sohyun <kshyun0215@gmail.com>
2022-07-05 23:45:01 +0900

    solve: DS - 10866 (#7)

---
+++ boj/sohyun/data_structure/10866/solution.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main(){
+  cin.tie(NULL);
+  ios::sync_with_stdio(0);
+  int n;
+  cin >> n;
+  deque<int> dq;
+  while(n--){
+    string cmd;
+    cin >> cmd;
+    if(cmd == "push_front"){
+      int num;
+      cin >> num;
+      dq.push_front(num);
+    }else if(cmd == "push_back"){
+      int num;
+      cin >> num;
+      dq.push_back(num);
+    }else if(cmd == "pop_front"){
+      if(dq.empty())  cout << -1 << '\n';
+      else{
+        cout << dq.front() << '\n';
+        dq.pop_front();
+      }
+    }else if(cmd == "pop_back"){
+      if(dq.empty())  cout << -1 << '\n';
+      else{
+        cout << dq.back() << '\n';
+        dq.pop_back();
+      }
+    }else if(cmd == "size"){
+      cout << dq.size() << '\n';
+    }else if(cmd == "empty"){
+      if(dq.empty())  cout << 1 << '\n';
+      else  cout << 0 << '\n';
+    }else if(cmd == "front"){
+      if(dq.empty())  cout << -1 << '\n';
+      else  cout << dq.front() << '\n';
+    }else if(cmd == "back"){
+      if(dq.empty())  cout << -1 << '\n';
+      else  cout << dq.back() << '\n';
+    }
+
+  }
+
+
+  return 0;
+}
--- boj/sohyun/data_structure/README.md
+++ boj/sohyun/data_structure/README.md
@@ -3,3 +3,6 @@
 |  1  | [스택 (10828)](https://www.acmicpc.net/problem/10828)|  ✅  |
 |  2  | [괄호 (9012)](https://www.acmicpc.net/problem/9012) |  ✅  |
 |  3  | [큐2 (18258)](https://www.acmicpc.net/problem/18258) |  ✅  |
+|  4  | [요세푸스 문제 (1158)](https://www.acmicpc.net/problem/1158) |  ✅  |
+|  5  | [카드2 (2164)](https://www.acmicpc.net/problem/2164) |  ✅  |
+|  6  | [덱 (10866)](https://www.acmicpc.net/problem/10866) |  ✅  |
